f4571bdc1a8bb9a941719ff5f2900d7a4fc75d02
Singuila01 <mbayechris201@gmail.com>
2024-11-21 20:51:42 +0100

    Modif page d'accueil

--- index.php
+++ index.php
@@ -9,7 +9,17 @@
     <?php include 'inc/header.php' ?>
 
     <section>
-        
+        <div class="article">
+            <h2>Présentation</h2>
+            <div class="zones">
+                <div class="zone">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur id cum cupiditate architecto deserunt ratione totam vitae! Iste sint, quisquam nisi nostrum ipsa, possimus corporis accusamus earum id quo voluptas corrupti eum molestiae. Fugiat, quasi non officia ex natus inventore dolorum fugit eum! Porro optio non aspernatur rem vel temporibus, atque, laboriosam cupiditate illo aut obcaecati dolorem consequuntur accusantium sint saepe hic autem numquam adipisci suscipit laudantium. Inventore, sunt. Qui dolorem laboriosam aut necessitatibus. Eveniet fugiat a quaerat nihil ab!</p>
+                </div>
+                <div class="zone">
+                    <img src="./assets/img/cartographer.png" alt="">
+                </div>
+            </div>
+        </div>
     </section>
 
 </body>
