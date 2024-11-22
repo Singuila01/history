0fee705bc056705f145835c9c01cfbd394aa8caf
Singuila01 <mbayechris201@gmail.com>
2024-11-22 22:51:02 +0100

    Ajout d'une classe pour la présentation

--- index.php
+++ index.php
@@ -11,12 +11,12 @@
     <section>
         <div class="article">
             <h2>Présentation</h2>
-            <div class="zones">
+            <div class="zones presen">
                 <div class="zone">
                     <p><span>Aventure Chronique</span> est une plateforme immersive et interactive où les utilisateurs peuvent vivre des aventures narratives captivantes. L’objectif est de plonger les utilisateurs dans des histoires qu’ils influencent eux-mêmes, en fonction des choix qu’ils effectuent. Le site propose un univers visuel attrayant, avec des graphismes soignés et une ambiance sonore adaptée à chaque scène pour renforcer l'immersion.</p>
                 </div>
                 <div class="zone">
-                    <img src="./assets/img/cartographer.png" alt="">
+                    <img src="./assets/img/rennes.jpg" alt="">
                 </div>
             </div>
         </div>
