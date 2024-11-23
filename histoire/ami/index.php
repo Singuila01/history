307012c3c9ee4c4b069e3d37aa4447e0681d7236
Singuila01 <mbayechris201@gmail.com>
2024-11-23 23:43:44 +0100

    Modif index ami

--- histoire/ami/index.php
+++ histoire/ami/index.php
@@ -4,8 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../../styles/styles.css">
 </head>
 <body>
-    
+    <?php include '../../inc/header.php' ?>
+
+    <section>
+        <div class="article">
+            <div class="zones histoire">
+                <div class="zone">
+                    <h2>123</h2>
+                    <p>123456789</p>
+                </div>
+                <div class="zone">
+                    <h2>123</h2>
+                    <p>123456789</p>
+                </div>
+                <div class="zone">
+                    <h2>123</h2>
+                    <p>123456789</p>
+                </div>
+                <div class="zone">
+                    <h2>123</h2>
+                    <p>123456789</p>
+                </div>
+                <div class="zone">
+                    <h2>123</h2>
+                    <p>123456789</p>
+                </div>
+                <div class="zone">
+                    <h2>123</h2>
+                    <p>123456789</p>
+                </div>
+                <div class="zone">
+                    <h2>123</h2>
+                    <p>123456789</p>
+                </div>
+                <div class="zone">
+                    <h2>123</h2>
+                    <p>123456789</p>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
