ef9d5f0c1ca24c650e2eaf61e43631ee4ee07386
Singuila01 <mbayechris201@gmail.com>
2024-11-24 11:02:36 +0100

    index ami

--- histoire/ami/index.php
+++ histoire/ami/index.php
@@ -7,9 +7,47 @@
     <link rel="stylesheet" href="../../styles/styles.css">
 </head>
 <body>
-    <?php include '../../inc/header.php' ?>
+    <?php include_once '../../inc/header.php' ?>
 
     <section>
+        <div class="article">
+            <div class="zones histoire">
+                <?php
+                        // $userId = $_SESSION['user_id']; 
+
+                        require_once '../../assets/config/database.php';
+                        $db = connectDB("localhost", "history", "root", "");
+                    
+                        // $sql = "SELECT * FROM utilisateurs WHERE id_user = '$userId' ";
+                        $sql = "SELECT * FROM histoires";
+                        $stmt = $db->prepare($sql);
+                        $stmt->execute();
+
+                        $liste_histoire = [];
+
+                        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                            $liste_histoire[] = $row;
+                        }
+
+                        if ($liste_histoire != null) {
+                            foreach ($liste_histoire as $histoire) {
+                                echo "<div class='zone'>";
+                                echo "<p>" . $histoire['libelle_histoire'] . "</p>";
+                                echo "</div>";
+                            }        
+                        } else {
+                            echo "<div class='zone'>";
+                            echo "<p>Il n'y a pas d'histoire</p>";
+                            echo "</div>"; 
+                        }
+                                       
+                    
+                ?>
+            </div>
+        </div>
+    </section>
+
+    <!-- <section>
         <div class="article">
             <div class="zones histoire">
                 <div class="zone">
@@ -46,6 +84,6 @@
                 </div>
             </div>
         </div>
-    </section>
+    </section> -->
 </body>
 </html>
