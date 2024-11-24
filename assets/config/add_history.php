03a323b058a0b0e4d40fe85511b5566de49cca1f
Singuila01 <mbayechris201@gmail.com>
2024-11-24 11:26:47 +0100

    Fichier pour ajouter une histoire

---
+++ assets/config/add_history.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../styles/styles.css">
+</head>
+<body>
+    <?php include_once '../../inc/header.php' ?>
+
+    <section>
+        <div class="article">
+            <div class="zones formulaire">
+                <h2>Ajouter une histoire</h2>
+                <div class="form">
+                    <form action="" method="post">
+                        <input type="text" name="titre" id=""><br>
+                        <textarea name="description" id=""></textarea>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
