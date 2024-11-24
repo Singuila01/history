ecaf6757d61d709086ba7c0e4258b99e8a07044d
Singuila01 <mbayechris201@gmail.com>
2024-11-24 11:53:27 +0100

    Modif ajout histoire

--- assets/config/add_history.php
+++ assets/config/add_history.php
@@ -14,9 +14,10 @@
             <div class="zones formulaire">
                 <h2>Ajouter une histoire</h2>
                 <div class="form">
-                    <form action="" method="post">
+                    <form action="add_history.php" method="post">
                         <input type="text" name="titre" id=""><br>
-                        <textarea name="description" id=""></textarea>
+                        <textarea name="description" id=""></textarea><br>
+                        <button type="submit">Ajouter une histoire</button>
                     </form>
                 </div>
             </div>
@@ -24,3 +25,21 @@
     </section>
 </body>
 </html>
+
+<?php
+require_once 'config/database.php';
+$db = connectDB("localhost", "moyenne_finale", "root", "");
+
+if (condition) {
+    $title = $_POST['titre'];
+    $description = $_POST['description'];
+    $id_type_histoire = $_POST[''];
+    $date_ajout = date("Y/m/d");
+
+    $sql = "INSERT INTO histoires VALUES ('$title', '$description', '$id_type_histoire', '$date_ajout')";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+}
+
+
+?>
